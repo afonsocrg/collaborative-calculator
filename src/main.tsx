d6a352bb1f4423327109031db79ed5c413e72b61
afonsocrg <afonso.crg@gmail.com>
2025-01-10 18:33:36 +0000

    users persist locally their ids, so that they can 'rejoin' sessions

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,27 @@
-import { StrictMode } from 'react'
-import { ReactTogether } from 'react-together'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ReactTogether } from "react-together";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const USER_ID_KEY = "userId";
+
+let userId = localStorage.getItem(USER_ID_KEY);
+if (!userId) {
+  userId = crypto.randomUUID();
+  localStorage.setItem(USER_ID_KEY, userId);
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ReactTogether sessionParams={{
-      appId: import.meta.env['VITE_APP_ID'],
-      apiKey: import.meta.env['VITE_API_KEY'],
-    }}>
+    <ReactTogether
+      sessionParams={{
+        appId: import.meta.env["VITE_APP_ID"],
+        apiKey: import.meta.env["VITE_API_KEY"],
+      }}
+      userId={userId}
+    >
       <App />
     </ReactTogether>
-  </StrictMode>,
-)
+  </StrictMode>
+);
